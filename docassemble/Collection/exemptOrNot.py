8f1571628493943b3500f0de237757348891fb94
brooksmk <mbrooks@gbls.org>
2018-12-15 17:03:44 -0500

    fix syntax errors in turn_income_weekly()

--- docassemble/Collection/exemptOrNot.py
+++ docassemble/Collection/exemptOrNot.py
@@ -95,17 +95,18 @@ class IncomeList(DAList):
 
 def turn_income_weekly(self):
     for i in self:
-        if self[i].period == "Monthly"
+        if self[i].period == "Monthly":
             self[i].amount == self[i].amount / 4.3
             self[i].period == "Weekly"
-        if self[i].period == "Yearly"
+        if self[i].period == "Yearly":
             self[i].amount == self[i].amount / 52
             self[i].period == "Weekly"
-        if self[i].period == "Weekly"
+        if self[i].period == "Weekly":
             pass
-        if self[i].period == "Twice per month"
-            self[i].period == "Weekly"
-        if self[i].period == "Once every two weeks"
+        if self[i].period == "Twice per month":
             self[i].amount == self[i].amount * .46
+            self[i].period == "Weekly"
+        if self[i].period == "Once every two weeks":
+            self[i].amount == self[i].amount * .5
             self[i].period == "Weekly"
 
